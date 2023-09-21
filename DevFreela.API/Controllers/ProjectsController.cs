5cac67b12ecf2c7f6856d31212c050ea15e0151c
Maynara Nachbar <may.nachbar@gmail.com>
2023-09-21 18:59:22 -0300

    feat: Created ProjectsController

---
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevFreela.API.Controllers
+{
+    [Route("api/projects")]
+    public class ProjectsController : ControllerBase
+    {
+       // api/projects?query=net core
+        [HttpGet]
+        // Buscar ou filtrar todos os projetos
+        public IActionResult Get(string query)
+        {
+            return Ok();
+        }
+    }
+}
